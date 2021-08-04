e05ea25b24194325fda508e304b62b92b846b340
Shubham Gantayat <sgantayat9@gmail.com>
2021-08-04 09:46:24 +0530

    added logs to login and signup

--- py_backend/login/login_user.py
+++ py_backend/login/login_user.py
@@ -5,17 +5,26 @@ from werkzeug.security import check_password_hash
 class Validation:
 
     def __init__(self, email, password):
-        self.email = email
-        self.password = password
+        try:
+            self.email = email
+            self.password = password
+        except Exception as e:
+            config.logger.log("ERROR", str(e))
 
     def check(self):
-        query = "select password,account from medhub.user where email = '" + self.email + "' allow filtering"
-        result = config.cassandra.session.execute(query).one()
-        if result is None:
-            return {"status": False, "message": "Email does not exist"}
-        else:
-            if check_password_hash(result[0], self.password):
-                return {"status": result[1], "message": "Login successful"}
+        try:
+            config.logger.log("INFO", "Searching for email in database...")
+            query = "select password,account from medhub.user where email = '" + self.email + "' allow filtering"
+            result = config.cassandra.session.execute(query).one()
+            if result is None:
+                config.logger.log("CRITICAL", "email not found")
+                return {"status": False, "message": "Email does not exist"}
             else:
-                return {"status": False, "message": "Wrong password"}
-
+                if check_password_hash(result[0], self.password):
+                    config.logger.log("INFO", "Login successful...")
+                    return {"status": result[1], "message": "Login successful"}
+                else:
+                    config.logger.log("WARNING", "Wrong password...")
+                    return {"status": False, "message": "Wrong password"}
+        except Exception as e:
+            config.logger.log("ERROR", str(e))
--- py_backend/signup/signup_api.py
+++ py_backend/signup/signup_api.py
@@ -1,10 +1,15 @@
 from py_backend.signup.signup_user import Registration
 from flask_restful import Resource
 from flask import request
+import config
 
 
 class Signup(Resource):
 
     def post(self):
-        record = request.get_json()['formData']
-        return Registration(record).insert_to_db()
+        try:
+            config.logger.log("INFO", "Starting registration process...")
+            record = request.get_json()['formData']
+            return Registration(record).insert_to_db()
+        except Exception as e:
+            config.logger.log("ERROR", str(e))
