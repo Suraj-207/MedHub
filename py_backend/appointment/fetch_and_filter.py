a90276b8167e8ca4bc9e30f18ffbe93235715969
Shubham Gantayat <sgantayat9@gmail.com>
2021-08-07 10:48:03 +0530

    patient home backend done

--- py_backend/appointment/fetch_and_filter.py
+++ py_backend/appointment/fetch_and_filter.py
@@ -2,7 +2,7 @@ import datetime
 import config
 
 
-class FetchFilterAppointment:
+class FetchFilter:
 
     @staticmethod
     def fetch_upcoming_appointment_by_doctor_email(email):
@@ -156,34 +156,87 @@ class FetchFilterAppointment:
                     next_date = datetime.date.fromisoformat(changes['date']) + datetime.timedelta(days=1)
                     next_date = next_date.isoformat()
                     date_condition = " and start >= '" + changes['date'] + "' and start < '" + next_date + "'"
-            query = "select doctor_email from medhub.appointment where patient_email = '" + patient_email + "' allow filtering;"
-            rows = config.cassandra.session.execute(query).all()
+            fetch_appt_info_query = "select * from medhub.appointment where patient_email = '" + patient_email + "'" + + date_condition + " allow filtering;"
+            fetch_appt_info = config.cassandra.session.execute(fetch_appt_info_query).all()
             res = []
-            for row in list(set(rows)):
-                fetch_name_query = "select fname,lname,speciality,experience from medhub.doctor where email = '" + row.doctor_email + "'"
+            for appt in fetch_appt_info:
+                fetch_name_query = "select fname,lname,speciality,experience from medhub.doctor where email = '" + appt.doctor_email + "'"
                 fetch_name = config.cassandra.session.execute(fetch_name_query).one()
-                appointments = []
-                fetch_appt_info_query = "select * from medhub.appointment where doctor_email = " + row.doctor_email + "' patient_email = '" + patient_email + "'" + date_condition + " order by start desc allow filtering;"
-                fetch_appt_info = config.cassandra.session.execute(fetch_appt_info_query).all()
-                for appt in fetch_appt_info:
-                    appointments.append({
-                        "fname": fetch_name.fname,
-                        "lname": fetch_name.lname,
-                        "speciality": fetch_name.speciality,
-                        "experience": fetch_name.experience,
-                        "patient_email": appt.patient_email,
-                        "start": appt.start,
-                        "session": appt.session,
-                        "appt_id": appt.appt_id,
-                        "diagnosis": appt.diagnosis,
-                        "prescription": appt.prescription,
-                        "status": appt.status,
-                        "issue": appt.issue
-                    })
-                res = res.extend(appointments)
+                res.append({
+                    "fname": fetch_name.fname,
+                    "lname": fetch_name.lname,
+                    "speciality": fetch_name.speciality,
+                    "experience": fetch_name.experience,
+                    "patient_email": appt.patient_email,
+                    "start": appt.start,
+                    "session": appt.session,
+                    "appt_id": appt.appt_id,
+                    "diagnosis": appt.diagnosis,
+                    "prescription": appt.prescription,
+                    "status": appt.status,
+                    "issue": appt.issue
+                })
             res = sorted(res, key=lambda x: x['start'], reverse=True)
             return res
         except Exception as e:
             config.logger.log("ERROR", str(e))
+
+    @staticmethod
+    def fetch_na_appointments(doctor_email):
+        try:
+            query = "select * from medhub.appointment where doctor_email = '" + doctor_email + "' and status = 'NA' order " \
+                                                                                               "by start asc "
+            res = []
+            fetch_name_query = "select fname,lname from medhub.doctor where email = '" + doctor_email + "'"
+            fetch_name = config.cassandra.session.execute(fetch_name_query).one()
+            for row in config.cassandra.session.execute(query).all():
+                res.append({
+                    "fname": fetch_name.fname,
+                    "lname": fetch_name.lname,
+                    "email": doctor_email,
+                    "start": row.start,
+                    "session": row.session,
+                    "appt_id": row.appt_id,
+                    "status": row.status,
+                    "issue": row.issue
+                })
+            return res
+        except Exception as e:
+            config.logger.log("ERROR", str(e))
+
+    @staticmethod
+    def fetch_filter_doctors(patient_email, changes):
+        try:
+            fetch_patient_info_query = "select city, state from medhub.patient where email = '" + patient_email + "'"
+            fetch_patient_info = config.cassandra.session.execute(fetch_patient_info_query).one()
+            speciality_condition = ''
+            if changes['speciality'] is not None:
+                speciality_condition = " and speciality = '" + changes['speciality'] + "'"
+            if changes['city'] is not None:
+                city_condition = " and city = '" + changes['city'] + "'"
+            else:
+                city_condition = " and city = '" + fetch_patient_info.city + "'"
+            if changes['state'] is not None:
+                state_condition = " and state = '" + changes['state'] + "'"
+            else:
+                state_condition = " and state = '" + fetch_patient_info.state + "'"
+            fetch_doctor_query = "select * from medhub.doctor where active = True and time_set = True" + state_condition + city_condition + speciality_condition + " allow filtering"
+            fetch_doctor = [{
+                "email": row.email,
+                "fname": row.fname,
+                "lname": row.lname,
+                "speciality": row.speciality,
+                "experience": row.experience,
+                "place_of_work": row.pow,
+                "city": row.city,
+                "state": row.state
+            } for row in config.cassandra.session.execute(fetch_doctor_query).all()]
+            fetch_doctor = sorted(fetch_doctor, key=lambda x: x['experience'], reverse=True)
+            return fetch_doctor
+        except Exception as e:
+            config.logger.log("ERROR", str(e))
+
+
+
 
 
