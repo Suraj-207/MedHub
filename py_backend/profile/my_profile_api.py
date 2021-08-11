6ceb6bdc3c1b64c23c2564032e489913c067d746
Suraj <sksuraj148333@gmail.com>
2021-08-11 13:00:27 +0530

    conflict backend

--- py_backend/profile/my_profile_api.py
+++ py_backend/profile/my_profile_api.py
@@ -1,8 +1,10 @@
-from flask import request
+from flask import request, send_file
 from flask_restful import Resource
 from py_backend.profile.my_profile import Profile
 import config
 from PIL import Image
+import io
+import base64
 
 
 class FetchProfile(Resource):
@@ -32,9 +34,19 @@ class ChangeProfile(Resource):
 class FetchImage(Resource):
 
     def post(self):
-        print(request.files)
-        img = request.files['image']
-        im = Image.open(img)
-        im.show()
+        print(0)
+        im = Image.open(request.files['image'])
+        file_object = io.BytesIO()
+        im.save(file_object,'PNG')
+        # im = Image.open(io.BytesIO(request.get_data()))
+        # im.save("hello.jpg")
+        converted_string = base64.b64encode(file_object.getvalue())
+        
+        return send_file(converted_string)
+        # print(request.get_json())
+        
+        # img = request.files['image']
+        # im = Image.open(img)
+        # im.show()
 
 
