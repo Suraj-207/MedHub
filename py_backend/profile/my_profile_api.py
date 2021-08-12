68d76e4216b311a4a63705ed51591ed93cff529e
Suraj <sksuraj148333@gmail.com>
2021-08-12 20:41:16 +0530

    pybackend

--- py_backend/profile/my_profile_api.py
+++ py_backend/profile/my_profile_api.py
@@ -33,19 +33,10 @@ class FetchImage(Resource):
 
     def post(self):
         img = request.files['image']
-<<<<<<< HEAD
-        im = Image.open(img)
-        raw_bytes = io.BytesIO()
-        im.save(raw_bytes, "PNG")
-        raw_bytes.seek(0)
-        img_base64 = base64.b64encode(raw_bytes.read()).decode('utf-8')
-        return img_base64
-=======
         decoded_img_data = ImageConvert().convert_to_b64(img)
         d = {'decoded': decoded_img_data}
         return d
 
->>>>>>> b0d55164cb932f2750b566aba90693e6c222b3bb
 
 
 
